4abc21eac1ba5a10dd5ab88adfafd43cacf39403
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2024-01-02 07:15:38 +0000

    new changes

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,18 @@
 import random
+
+def write_quotes():
+   new_quotes = [
+        "To be or not to be, that is the question.",
+        "All that glitters is not gold.",
+        "The only thing we have to fear is fear itself.",
+        "In three words I can sum up everything I've learned about life: it goes on."
+   ]
+
+   with open("quotes.txt", "a") as f:
+      for quote in new_quotes:
+         f.write(quote + "\n")
+   
+
 def primary():
    #print("Keep it logically awesome.")
 
@@ -6,9 +20,16 @@ def primary():
    quotes = f.readlines()
    f.close()
    
-   last = 13
+   last = len(quotes) - 1
+   # print(last)
    rnd = random.randint(0, last)
-   print(quotes[rnd])
+   # print(quotes[rnd])
+   print(quotes[rnd].strip())
+   # for i in quotes:
+   #    print(i.strip())
+      
 
 if __name__== "__main__":
-  primary()
+
+   write_quotes()
+#   primary()
